a2b8331001c6666636399b736371437ee7819d0d
vadde suresh <sureshvadde1076@gmail.com>
2022-12-12 10:39:22 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args){
         RWOnly obj = new RWOnly();
-        obj.name = "suresh";
+        obj.setName("suresh");
+        obj.getName();
 
     }
   
